604dfb87b57692d936388f1ee5f459d4977664a9
Ausra <munduswebstudio@gmail.com>
2023-07-11 10:21:01 +0300

    crud, messages

--- miskas/routes/web.php
+++ miskas/routes/web.php
@@ -36,9 +36,15 @@ Route::post('/calculator', [C::class, 'doCalculator'])->name('do-calculator');
 
 // Route grupavimas
 Route::prefix('colors')->name('colors-')->group(function () {
-    Route::get('/', [R::class, 'index'])->name('index'); // GET /colors from URL: colors Name: colors-index
-    Route::get('/create', [R::class, 'create'])->name('create'); // GET /colors/create from URL: colors/create Name: colors-create
+
+    Route::get('/', [R::class, 'index'])->name('index'); // GET /colors from URL:  colors Name: colors-index
+    Route::get('/create', [R::class, 'create'])->name('create'); // GET /colors/create from URL:  colors/create Name: colors-create
     Route::post('/', [R::class, 'store'])->name('store'); // POST /colors from URL:  colors Name: colors-store
+    Route::get('/delete/{color}', [R::class, 'delete'])->name('delete'); // GET /colors/delete/{color} from URL:  colors/delete/{color} Name: colors-delete
+    Route::delete('/{color}', [R::class, 'destroy'])->name('destroy'); // DELETE /colors/{color} from URL:  colors/{color} Name: colors-destroy 
+    Route::get('/edit/{color}', [R::class, 'edit'])->name('edit'); // GET /colors/edit/{color} from URL:  colors/edit/{color} Name: colors-edit
+    Route::put('/{color}', [R::class, 'update'])->name('update'); // PUT /colors/{color} from URL:  colors/{color} Name: colors-update
+
 });
 
 
